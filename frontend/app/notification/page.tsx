4e815faaf4fe54e56e79a54fe97da5ca9a77f696
Ashutosh <ashutoshkr.8920@gmail.com>
2026-03-30 15:00:55 +0530

    feat: update NotificationsPage to include image URLs for items and enhance layout with flexbox

--- frontend/app/notification/page.tsx
+++ frontend/app/notification/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { useAuth } from "@clerk/nextjs"
 import Header from "@/components/Header";
 
 const dummyItems = [
@@ -9,16 +8,19 @@ const dummyItems = [
     id: 1,
     name: "Milk",
     expiry_date: "2026-03-27",
+    image_url: "https://t4.ftcdn.net/jpg/05/35/38/81/360_F_535388138_vrJV0SoQBPc6oUyHeIFQkw8nFaH1GP3O.jpg"
   },
   {
     id: 2,
     name: "Tomato",
     expiry_date: "2026-03-28",
+    image_url: "https://t4.ftcdn.net/jpg/05/72/95/29/360_F_572952963_YNVwHoPX0FWaENu6jShGiYpIICS3W1cc.jpg"
   },
   {
     id: 3,
     name: "Cheese",
     expiry_date: "2026-03-26",
+    image_url: "https://images.unsplash.com/photo-1683314573422-649a3c6ad784?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlZXNlfGVufDB8fDB8fHww"
   },
 ];
 
@@ -27,9 +29,7 @@ export default function NotificationsPage() {
   const [items, setItems] = useState<any[]>([])
 
   useEffect(() => {
-
     setItems(dummyItems)
-
   }, [])
 
 
@@ -52,8 +52,16 @@ export default function NotificationsPage() {
 
         <div
           key={item.id}
-          className="border rounded-xl p-3 mb-3"
+          className="border rounded-xl p-3 mb-3 flex gap-3"
         >
+          <div className="w-14 h-14  flex items-center rounded-full">
+         <img 
+  src={item.image_url} 
+  alt={item.name} 
+  className="w-14 h-14 object-cover rounded-full"
+/>
+          </div>
+          <div>
 
           <h3 className="font-semibold">
             {item.name}
@@ -62,6 +70,7 @@ export default function NotificationsPage() {
           <p className="text-sm text-gray-500">
             expires: {item.expiry_date}
           </p>
+          </div>
 
         </div>
 
